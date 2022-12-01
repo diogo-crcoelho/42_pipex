75edbee092d5057646bac4a5f2699ec92c3ed623
dcarvalh <dcarvalh@student.42lisboa.com>
2022-12-01 14:17:15 +0000

    kinda good

--- Makefile
+++ Makefile
@@ -22,29 +22,30 @@ SRCS =  main.c \
 OBJS = $(SRCS:.c=.o)
 
 C_RED = \033[0;31m
-C_GREEN = \033[0;32m
+C_GREEN = \033[1;92m
 C_RESET = \033[0m
-C_ORANGE = \033[0;33m
+C_PURPLE = \033[0;35m
 C_RED = \033[0;31m
 
+echo = /bin/echo -e
 %.o:%.c
 	@$(CC) $(CFLAGS) -c $< -o $@
-	@echo "$(C_GREEN) [OK]   $(C_ORANGE) Compiling:$(C_RESET)" $<
+	@$(echo) "$(C_GREEN) [OK]   $(C_PURPLE) Compiling:$(C_RESET)" $<
 
 $(NAME): $(OBJS)
 	@$(CC) $(OBJS) -o $(NAME)
-	@echo "$(C_GREEN) [OK]   $(C_ORANGE) Compiling:$(C_RESET)" $(NAME)
-	@echo "$(C_GREEN)\tCompiled $(NAME)$(C_RESET)"
+	@$(echo) "$(C_GREEN) [OK]   $(C_PURPLE) Compiling:$(C_RESET)" $(NAME)
+	@$(echo) "$(C_GREEN)\tCompiled $(NAME)$(C_RESET)"
 	
 all : $(NAME)
 
 clean:
 	@rm -f $(OBJS) *.txt
-	@echo "$(C_RED)\tRemoved object files$(C_RESET)"
+	@$(echo) "$(C_RED)\tRemoved object files$(C_RESET)"
 	
 fclean: clean
 	@rm -f $(NAME)
-	@echo "$(C_RED)\tRemoved $(NAME)$(C_RESET)"
+	@$(echo) "$(C_RED)\tRemoved $(NAME)$(C_RESET)"
 	
 re: fclean all
 	
--- main.c
+++ main.c
@@ -21,32 +21,51 @@ void	free_env(t_envs *env)
 	free(env->cmds);
 	free_path(env->paths);
 }
-
+//-1- invalid parameter
+//1- malloc error
 void	err_handle(int error, t_envs *env)
 {
 	if (error == -1)
 		write(2, "\tInvalid number of parameters", 30);
-	else if (env->fds[0] < 0)
+	else if (error == 2)
+	{
 		perror(env->files[0]);
-			exit(-1);
+		return ;
+	}
+	else if (error == 3)
+		perror(env->files[1]);
+	exit(-1);
 }
 
-void	make_env(void)
+void	make_env(int argc, char **argv, char **envp, t_envs *env)
 {
-	
+	(void) envp;
+	env->cmds = parse_commands(argc, argv);
+	if (!(env->cmds))
+		err_handle(1, env);
+	env->paths = get_path(envp, "PATH", -1, 5);
+	if (!(env->paths))	
+		err_handle(1, env);
+	env->files[0] = argv[1];
+	env->files[1] = argv[argc - 1];
+	env->fds[0] = open(argv[1], O_RDONLY);
+	if (env->fds[0] < 0)
+		err_handle(2, env);
+	env->fds[1] = open(argv[argc - 1], O_RDWR | O_CREAT | O_TRUNC, 0666);
+	if (env->fds[1] < 0)
+		err_handle(3, env);
+
 }
+
 int	main(int argc, char **argv, char **envp)
 {
 	t_envs	env;
-
+	(void)envp;
 	if (argc != 5)
 		err_handle(-1, &env);
-	env.fds[0] = open(argv[1], O_RDONLY);
-	env.fds[1] = open(argv[argc - 1], O_RDWR | O_CREAT | O_TRUNC, 0666);
-	if (env.fds[0] < 0 || env.fds[1] < 0)
-		err_handle(0, &env);
-	env.paths = get_path(envp, "PATH", -1, 5);
-	env.cmds = parse_commands(argc, argv);
+	make_env(argc, argv, envp, &env);
+	printf("%s\n", env.paths[0]);
+	printf("%s\n", env.cmds[0]);
 	free_env(&env);
 	return (0);
 }
