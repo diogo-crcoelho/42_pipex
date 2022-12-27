48abd59b2ed5325411e078b351a8fb840930ccd0
dcarvalh <dcarvalh@student.42lisboa.com>
2022-12-27 17:12:54 +0000

    .h reorganized

--- incs/pipex_bonus.h
+++ incs/pipex_bonus.h
@@ -34,16 +34,16 @@ typedef struct s_doraemon
 	char			**envp;
 }					t_envs;
 
+char	*get_next_line(int fd);
 char	*get_path(char **env, char *str);
 char	**ft_split(char *str, int sep);
+int		ft_strcmp(char *s1, char *s2);
 void	close_pipes(int fds[2]);
 void	err_handle(char *str, int code);
 void	free_cmds(t_cmd *head);
 void	free_pp(char **pp, int i);
-t_cmd	**parse_cmds(t_cmd **head, char **argv, char **envp, int idx);
-char	*get_next_line(int fd);
-int		ft_strcmp(char *s1, char *s2);
-void	prot_dup2(int fd1, int fd2);
 void	make_here_env(int argc, char **argv, t_envs *env);
+void	prot_dup2(int fd1, int fd2);
+t_cmd	**parse_cmds(t_cmd **head, char **argv, char **envp, int idx);
 
 #endif
